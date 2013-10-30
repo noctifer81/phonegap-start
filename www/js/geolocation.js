4307f2d642f62a7d2256e93c94c8077338bdf060
Giancarlo <me@giancarlodossini.com>
2013-10-31 00:24:20 +0100

    Add google maps

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -14,7 +14,8 @@ navigator.geolocation.getCurrentPosition(onSuccess, onError);
                             'Heading: '            + position.coords.heading               + '<br />' +
                             'Speed: '              + position.coords.speed                 + '<br />' +
                             'Timestamp: '          + position.timestamp                    + '<br />';
-    }
+    	inizialize (position.coords.latitude, position.coords.longitude);
+	}
 
     // onError Callback receives a PositionError object
     //
@@ -22,3 +23,21 @@ navigator.geolocation.getCurrentPosition(onSuccess, onError);
         alert('code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
     }
+	
+	function initialize( lat, long) {
+  var myLatlng = new google.maps.LatLng(lat,long);
+  var mapOptions = {
+    zoom: 6,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
