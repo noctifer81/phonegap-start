5ec526a83d70e76ffd9e2447d422cbe85258e4d2
Giancarlo <me@giancarlodossini.com>
2013-10-31 09:24:45 +0100

    Geolocation

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -25,19 +25,19 @@ navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
 	
 	function initialize( lat, long) {
-  var myLatlng = new google.maps.LatLng(lat,long);
-  var mapOptions = {
-    zoom: 6,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Hello World!'
-  });
-}
+	  var myLatlng = new google.maps.LatLng(lat,long);
+	  var mapOptions = {
+		zoom: 6,
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	  }
+	  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	
+	  var marker = new google.maps.Marker({
+		  position: myLatlng,
+		  map: map,
+		  title: 'Hello World!'
+	  });
+	}
 
 google.maps.event.addDomListener(window, 'load', initialize);
